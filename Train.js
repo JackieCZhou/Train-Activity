bf3bddd2dd32791a530eca853c6d968237a6b876
Jackie Davison <jackiedavison@Jackies-MacBook-Air.local>
2019-04-22 15:12:31 -0400

    fixed Train act

--- Train.js
+++ Train.js
@@ -22,10 +22,10 @@ $("#submit").on("click", function(event) {
         destination: destEntered,
         startTime: startEntered,
         frequency: freqEntered,
+        // minutesAway: timeLeft,
         
     })
-    // minAway = startTime - frequency???
-    // var start = 
+
 
 });
 
@@ -44,11 +44,27 @@ newEntry.on("child_added", function(childSnapshot) {
     var trainTd = $("<td>").text(newAdd.train);
     var destinationTd = $("<td>").text(newAdd.destination);
     var frequencyTd = $("<td>").text(newAdd.frequency);
-    var minAwayTd = moment().format('hh:mm');
-    var nextTd   =
 
-    // var nextTd = moment().from(start);
-    tRow.append(trainTd, destinationTd, frequencyTd, nextTd, minAwayTd);
+    
+    var currentTime = moment();
+    var firstTime = newAdd.startTime;
+    var frequency = newAdd.frequency;
+    // difference in time between when 1st train left & current time
+    var diffTime = moment().diff(moment(firstTime, "HH:mm"), "minutes");
+    console.log(diffTime);
+    //remainder = minutes into last train (ie: train is on a 32 min trip, it's 28 mins into trip, there's 4 minutes left, 28 is the num calc)
+    var timePassed = diffTime % frequency;
+    // minutes difference from amount of time train has "traveled" to amount of time before it's arrival
+    var timeLeft = frequency - timePassed;
+    console.log(timeLeft)
+    var nextTrain = moment().add(timeLeft, "minutes");
+    var nextTrainTime = $("<td>").text(moment(nextTrain).format("hh:mm"));
+    console.log(nextTrain);
+    var minAwayTd = $("<td>").text(timeLeft);
+
+
+
+    tRow.append(trainTd, destinationTd, frequencyTd, nextTrainTime, minAwayTd);
     tBody.append(tRow);
     $("#table").append(tBody);
 
@@ -57,9 +73,3 @@ newEntry.on("child_added", function(childSnapshot) {
     
 
 });
-
-// can't get the Next Arrival column to display anything other than the current time, regardless of what I put in the form.  
-// unable to figure out the minutes away column.  
-// Tried reading the documentation on it but coudln't figure out how to get it to display/what the 
-// equation was to get a number to appear in that spot.
-// unsure of why everytime I reload the page the timestamp updates to the current time.
